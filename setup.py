0ef210230d02606813c81c52f6d71d1c85de863b
Narendra Khudania <narendrakhudania@Narendras-MacBook-Pro.local>
2026-02-04 15:03:27 +0530

    Langchain

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="finance-chatbot",
+    version="0.1.0",
+    author="Narendra Khudania",
+    author_email="narendrakhudania@gmail.com",
+    packages=find_packages(),
+    python_requires=">=3.10",
+)
